3d9f375f7c816d7fb04e7654519dfb10c44427fb
xihalameilin <1293086146@qq.com>
2019-05-23 13:53:26 +0800

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <Button type="primary" @click="notify">原始的按钮</Button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {}
+        },
+      methods:{
+        notify(){
+            alert("notify")
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
