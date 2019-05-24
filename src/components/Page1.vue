3dcedae8e57d679721a2683af2a98620f71aaafb
xihalameilin <1293086146@qq.com>
2019-05-24 17:09:29 +0800

    5-24 17：09

---
+++ src/components/Page1.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <Input search enter-button placeholder="Enter something..." v-model="keyword"  style="float: right;width: 30%" on-enter=""/>
+
+    <Table border  :columns="columns" :data="data" style="width: 120em"></Table>
+  </div>
+
+</template>
+<script>
+  export default {
+
+    created(){
+      
+    },
+    data () {
+      return {
+        keyword:'',
+        columns: [
+          {
+            title: '姓名',
+            key: 'name',
+            render: (h, params) => {
+              var self =this
+              return h('div', [
+                  h('Icon', {
+                    props: {
+                      type: 'ios-person'
+                    }
+                  }),
+                  h('span', {
+                    domProps: {
+                      innerHTML: params.row.name.replace(self.keyword, '<span style="background: #ffff00;">'+self.keyword+'</span>')
+                    }
+                  })
+                ],
+              );
+            }
+
+          },
+          {
+            title: '年龄',
+            key: 'age'
+          },
+          {
+            title: '地址',
+            key: 'address'
+          },
+          {
+            title: '操作',
+            key: 'action',
+            width: 300,
+            align: 'center',
+            render: (h, params) => {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginLeft: '5px',
+
+                  },
+                  on: {
+                    click: () => {
+
+                    }
+                  }
+                }, '进入店铺'),
+
+                h('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  style: {
+                    marginLeft: '5px',
+
+                  },
+                  on: {
+                    click: () => {
+
+                    }
+                  }
+                }, '进入店铺'),
+
+              ]);
+            }
+          }
+        ],
+        data: [
+          {
+            name: '王小明',
+            age: 18,
+            address: '北京市朝阳区芍药居'
+          },
+          {
+            name: '张小刚',
+            age: 25,
+            address: '北京市海淀区西二旗'
+          },
+          {
+            name: '李小红',
+            age: 30,
+            address: '上海市浦东新区世纪大道'
+          },
+          {
+            name: '周小伟',
+            age: 26,
+            address: '深圳市南山区深南大道'
+          }
+        ]
+      }
+    },
+    methods: {
+      show (index) {
+        this.$Modal.info({
+          title: '用户信息',
+          content: `姓名：${this.data6[index].name}<br>年龄：${this.data6[index].age}<br>地址：${this.data6[index].address}`
+        })
+      },
+      remove (index) {
+        this.data.splice(index, 1);
+      }
+    }
+  }
+</script>
---
+++ src/components/Page2.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Menu :theme="theme3" active-name="1" style="float: left">
+      <MenuGroup title="内容管理">
+        <MenuItem name="1">
+          <Icon type="md-document" />
+          <router-link to="/page21">用户管理</router-link>
+        </MenuItem>
+        <MenuItem name="2">
+          <Icon type="md-chatbubbles" />
+          评论管理
+        </MenuItem>
+      </MenuGroup>
+      <MenuGroup title="统计分析">
+        <MenuItem name="3">
+          <Icon type="md-heart" />
+          用户留存
+        </MenuItem>
+        <MenuItem name="4">
+          <Icon type="md-leaf" />
+          流失用户
+        </MenuItem>
+      </MenuGroup>
+    </Menu>
+    <router-view style="float: left"></router-view>
+  </div>
+
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        theme3: 'light'
+      }
+    }
+  }
+</script>
